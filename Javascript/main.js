9c481349e383d532c8ece74307dec1a761ea7247
Lionel Crafts <lionel.crafts@gmail.com>
2021-04-04 08:32:21 -0500

    Added a debug mode setting in prep for creating server rooms

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,8 +1,30 @@
 let Main = {
+    /**
+     * Debug properties
+     */
     clientWidget: WidgetHelpers.createClient(WidgetShape),
     serverWidget: WidgetHelpers.createServer(WidgetShape),
 
     initialize: function() {
+        if (Settings.debugMode) {
+            this._initializeForDebug();
+        }
+    },
+
+    /**
+     * 
+     */
+    _initializeForDebug: function() {
+        let header = dce("h1");
+        header.innerText = "Figure out the answer";
+
+        let testButton = dce("button");
+        testButton.innerText = "Check"
+        testButton.onclick = this.testButtonClick.bind(this);
+
+        document.body.appendChild(header);
+        document.body.appendChild(testButton);
+
         document.body.appendChild(this.clientWidget.div);
         //document.body.appendChild(this.serverWidget.div);
     },
---
+++ Javascript/settings.js
@@ -0,0 +1,3 @@
+let Settings = {
+    debugMode: true
+}
